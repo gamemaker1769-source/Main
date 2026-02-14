fc89504a99e71e592223071b50cba6b74a38c565
SIam <gamemaker1769@gmail.com>
2026-02-14 22:09:30 +0600

    Change bot off message emoji from '🛑' to '🔴'

--- scripts/cmds/off.js
+++ scripts/cmds/off.js
@@ -3,13 +3,13 @@ module.exports = {
 		name: "off",
 		version: "1.0",
 		author: "Siam",
-		role: 2, // 2 = admin (change to 0 if everyone can use)
+		role: 2,
 		category: "system",
 		shortDescription: "Turn bot off"
 	},
 
 	onStart: async function ({ message }) {
 		global.GoatBot.botStatus = false;
-		return message.reply("🛑 Bot is now OFF.");
+		return message.reply("🔴 Bot is now OFF.");
 	}
 };
